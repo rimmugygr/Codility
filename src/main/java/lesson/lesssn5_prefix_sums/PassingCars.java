96c36cfef8ccc94e34acd33f089ee0209eefaee6
rimmugyr <rimmugygr@gmail.com>
2020-03-02 12:40:39 +0100

    add-log4j-and-config-then

--- src/main/java/lesson/lesssn5_prefix_sums/PassingCars.java
+++ src/main/java/lesson/lesssn5_prefix_sums/PassingCars.java
@@ -1,11 +1,12 @@
 package lesson.lesssn5_prefix_sums;
 
-import java.util.Arrays;
+
+import java.util.*;
+import java.util.logging.Logger;
 
 /**
- *A non-empty array A consisting of N integers is given.
+ * A non-empty array A consisting of N integers is given.
  * The consecutive elements of array A represent consecutive cars on a road.
- *
  * Array A contains only 0s and/or 1s:
  *
  * 0 represents a car traveling east,
@@ -15,11 +16,11 @@ import java.util.Arrays;
  *
  * For example, consider array A such that:
  *
- *   A[0] = 0
- *   A[1] = 1
- *   A[2] = 0
- *   A[3] = 1
- *   A[4] = 1
+ * A[0] = 0
+ * A[1] = 1
+ * A[2] = 0
+ * A[3] = 1
+ * A[4] = 1
  * We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
  *
  * Write a function:
@@ -32,111 +33,58 @@ import java.util.Arrays;
  *
  * For example, given:
  *
- *   A[0] = 0
- *   A[1] = 1
- *   A[2] = 0
- *   A[3] = 1
- *   A[4] = 1
+ * A[0] = 0
+ * A[1] = 1
+ * A[2] = 0
+ * A[3] = 1
+ * A[4] = 1
  * the function should return 5, as explained above.
  *
  * Write an efficient algorithm for the following assumptions:
  *
  * N is an integer within the range [1..100,000];
  * each element of array A is an integer that can have one of the following values: 0, 1
- *
- * */
+ */
 
 public class PassingCars {
+    protected final static org.apache.log4j.Logger LOG= org.apache.log4j.Logger.getLogger(PassingCars.class);
     public static void main(String[] args) {
-        int[] A = new int[7];
-        A[0] = 4;
-        A[1] = 2;
-        A[2] = 2;
-        A[3] = 5;
+        int[] A = new int[5];
+        A[0] = 0;
+        A[1] = 1;
+        A[2] = 0;
+        A[3] = 1;
         A[4] = 1;
-        A[5] = 5;
-        A[6] = 8;
-        System.out.println(solutionOld(A)+"<<<");
+        System.out.println(solution(A)+"<<<");
     }
     public static int  solution(int[] tabInput) {
-        float[] avarageA=new float[tabInput.length-1];
-        float[] avarageB=new float[tabInput.length-1];
 
-        int sum=tabInput[0];
-        for (int i = 1; i < tabInput.length; i++) {
-            avarageA[i-1]=sum+tabInput[i];
-            sum= (int) avarageA[i-1];
-            avarageA[i-1]/=i+1;
-        }
-
-        sum=tabInput[tabInput.length-1];
-        for (int i = tabInput.length - 2; i >= 0; i--) {
-            avarageB[i]=sum+tabInput[i];
-            sum= (int) avarageB[i];
-            avarageB[i]/=tabInput.length-i;
-        }
-
-        System.out.println(Arrays.toString(avarageA));
-        System.out.println(Arrays.toString(avarageB));
-        System.out.println(Arrays.toString(tabInput));
-
-
-        int[] resultTab=new int[tabInput.length-1];
-
-        for (int i = 0; i < resultTab.length; i++) {
-            for (int j = 0; j < resultTab.length; j++) {
-
+        int[] resultTab = new int[tabInput.length];
+        int eastCar = 0;
+        int westCar = 0;
+       // int temp = 100_000;
+        for (int i = tabInput.length - 1; i >= 0; i--) {
+            if (tabInput[i]==0){
+                resultTab[eastCar]=westCar;
+                eastCar++;
+            }else {
+                westCar++;
             }
+
+            LOG.info("****");
+            LOG.info(eastCar);
+            LOG.info(westCar);
+          //  LOG.info(temp);
+            LOG.info(Arrays.toString(resultTab));
+
+
         }
-
-
-
-
-        int minValue=Integer.MAX_VALUE;
         int result=0;
-        for (int i = resultTab.length - 1; i >= 0; i--) {
-            if (resultTab[i]<=minValue){
-                minValue=resultTab[i];
-                result=i;
-            }
+        for (int i = 0; i < eastCar; i++) {
+            result+=resultTab[i];
         }
-
-
-
-
 
         return result;
-    }
-
-    public static int solutionOld(int[] tabInput) {
-        float[] resultTab=new float[tabInput.length-1];
-
-        System.out.println(Arrays.toString(tabInput));
-        for (int i = 0; i < tabInput.length-1; i++) {
-            int sum=tabInput[i];
-            resultTab[i]=10_000;
-
-            for (int j = i; j < tabInput.length-1; j++) {
-                sum=sum+tabInput[j+1];
-                float temp=(float) sum/(j+2);
-                System.out.println(temp);
-                if (resultTab[i]>temp) resultTab[i]=temp;
-                System.out.println(Arrays.toString(resultTab));
-            }
-        }
-
-
-        int minValue=Integer.MAX_VALUE;
-        int result=0;
-        for (int i = resultTab.length - 1; i >= 0; i--) {
-            if (resultTab[i]<=minValue){
-                minValue=(int) resultTab[i];
-                result=i;
-            }
-        }
-
-
-        return tabInput[result];
     }
 
 }
