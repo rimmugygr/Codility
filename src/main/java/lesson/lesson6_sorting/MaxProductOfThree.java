e8f0774d34263287b47640fb7342f6da2d3cb636
rimmugyr <rimmugygr@gmail.com>
2020-03-02 15:52:09 +0100

    lesson6-MaxProductOfThree version2-100%

--- src/main/java/lesson/lesson6_sorting/MaxProductOfThree.java
+++ src/main/java/lesson/lesson6_sorting/MaxProductOfThree.java
@@ -1,9 +1,7 @@
 package lesson.lesson6_sorting;
 
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * A non-empty array A consisting of N integers is given. The product of triplet (P, Q, R) equates to A[P] * A[Q] * A[R] (0 ≤ P < Q < R < N).
@@ -60,10 +58,20 @@ public class MaxProductOfThree {
         int[] tab125 = {-5, 5, -5, 4};
         int[] tabMinus120 ={-5, -6, -4, -7, -10};
         int[] tab105 = {4, 7, 3, 2, 1, -3, -5};
-        System.out.println(solution(A)+"<<<");
-        System.out.println(solution(tab125)+"<<<");
-        System.out.println(solution(tabMinus120)+"<<<");
-        System.out.println(solution(tab105)+"<<<");
+        int[] tab0 = {-2, -3, -5, -6, 0};
+        LOG.info(solution(A)+"<<<");
+        LOG.info(solution(tab125)+"<<<");
+//        LOG.info(solution(tabMinus120)+"<<<");
+//        LOG.info(solution(tab105)+"<<<");
+//        LOG.info(solution(tab0)+"<<<");
+        LOG.info("***");
+
+        LOG.info(solutionV2(A)+"<<<");
+        LOG.info(solutionV2(tab125)+"<<<");
+//        LOG.info(solutionV2(tabMinus120)+"<<<");
+//        LOG.info(solutionV2(tab105)+"<<<");
+//        LOG.info(solutionV2(tab0)+"<<<");
+
     }
     public static int  solution(int[] tabInput) {
 
@@ -127,11 +135,6 @@ public class MaxProductOfThree {
             return plusMax[0]*plusMax[1]*plusMax[2];
        }
 
-
-
-
-
-
         //-+-
         if (plusMax[2]<=-minusMax[2] && plusMax[2]>=-minusMax[1] && plusMax[1]<=-minusMax[1])
             return minusMax[2]*plusMax[2]*minusMax[1];
@@ -145,5 +148,89 @@ public class MaxProductOfThree {
         return plusMax[0]*plusMax[1]*plusMax[2];
     }
 
+    public static int  solutionV2(int[] tabInput) {
 
+        int[] plusMax={-1,-1,-1};
+        int[] minusMax={1,1,1};
+
+        for (int i = 0; i < tabInput.length; i++) {
+
+            if (tabInput[i]>=plusMax[0]){
+                if (tabInput[i]>plusMax[2]){
+                    plusMax[0]=plusMax[1];
+                    plusMax[1]=plusMax[2];
+                    plusMax[2]=tabInput[i];
+                } else if (tabInput[i]>plusMax[1]){
+                    plusMax[0]=plusMax[1];
+                    plusMax[1]=tabInput[i];
+                } else {
+                    plusMax[0]=tabInput[i];
+                }
+                LOG.info(Arrays.toString(plusMax));
+            }
+
+            if (tabInput[i]<minusMax[0]){
+                if (tabInput[i]<minusMax[2]){
+                    minusMax[0]=minusMax[1];
+                    minusMax[1]=minusMax[2];
+                    minusMax[2]=tabInput[i];
+                } else if (tabInput[i]<minusMax[1]){
+                    minusMax[0]=minusMax[1];
+                    minusMax[1]=tabInput[i];
+                } else {
+                    minusMax[0]=tabInput[i];
+                }
+                LOG.info(Arrays.toString(minusMax));
+            }
+
+        }
+
+        //+++
+        if (minusMax[0]==1 && minusMax[1]==1 && minusMax[2]==1)
+            return plusMax[0]*plusMax[1]*plusMax[2];
+        //---
+        if (plusMax[0]==-1 && plusMax[1]==-1 && plusMax[2]==-1){
+            plusMax[2]=tabInput[0];
+            plusMax[1]=Integer.MIN_VALUE;
+            plusMax[0]=Integer.MIN_VALUE;
+
+            for (int i = 1; i < tabInput.length; i++) {
+                if (tabInput[i]>plusMax[2]){
+                    plusMax[0]=plusMax[1];
+                    plusMax[1]=plusMax[2];
+                    plusMax[2]=tabInput[i];
+                } else if (tabInput[i]>plusMax[1]){
+                    plusMax[0]=plusMax[1];
+                    plusMax[1]=tabInput[i];
+                } else if (tabInput[i]>plusMax[0]){
+                    plusMax[0]=tabInput[i];
+                }
+                LOG.info(Arrays.toString(plusMax) +" * ");
+            }
+            return plusMax[0]*plusMax[1]*plusMax[2];
+        }
+
+        List<Integer> temp = new ArrayList<>();
+        for (int i : minusMax) {
+            if (i<0) temp.add(i);
+        }
+        for (int i : plusMax) {
+            if (i>=0) temp.add(i);
+        }
+        LOG.info(temp.toString()+"@");
+
+        int sum=Integer.MIN_VALUE;
+        int value;
+        for (int i = 0; i < temp.size()-2; i++) {
+            for (int j = i+1; j < temp.size()-1; j++) {
+                for (int k = j+1; k < temp.size(); k++) {
+                    value=temp.get(i)*temp.get(j)*temp.get(k);
+                    if (sum<value)sum=value;
+                }
+            }
+        }
+
+
+        return sum;
+    }
 }
