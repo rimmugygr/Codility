4019911e44befe33a758e2516e19d2a62545cdbe
rimmugyr <rimmugygr@gmail.com>
2020-03-10 13:39:12 +0100

    lesson9-MaxSliceSum-83% performance-60% version2 TODO

--- src/main/java/lesson/lesson9_maximum_slice_problem/MaxSliceSum.java
+++ src/main/java/lesson/lesson9_maximum_slice_problem/MaxSliceSum.java
@@ -31,21 +31,56 @@ import org.apache.log4j.Logger;
  */
 
 public class MaxSliceSum {
-    protected final static Logger LOG= Logger.getLogger(MaxSliceSum.class);
+    protected final static Logger LOG = Logger.getLogger(MaxSliceSum.class);
+
     public static void main(String[] args) {
 
-        int[] A=new int[6];
-        A[0] = 3;  A[1] = 2;  A[2] = -6;
-        A[3] = 4;  A[4] = 0;
-        LOG.info(solution(A));
-        LOG.info(solution(new int[]{1}));
+        int[] A = new int[5];
+        A[0] = 3;
+        A[1] = 2;
+        A[2] = -6;
+        A[3] = 4;
+        A[4] = 0;
+        LOG.info(solutionV1(A));
+        LOG.info(solutionV1(new int[]{1,-1}));
+        LOG.info(solutionV1(new int[]{1,2,3}));
+        LOG.info("===========");
+        LOG.info(solutionV2(A));
+        LOG.info(solutionV2(new int[]{1,-1}));
+        LOG.info(solutionV2(new int[]{1,2,3}));
     }
-    public static int  solution(int[] input) {
-        if(input.length==1) return input[0];
-        int maxSum=Integer.MIN_VALUE;
-        int actualSum=0;
+
+    public static int solutionV2(int[] input) {
+        //dla 1 elentu suma zwsze warością tego elementu
+        if (input.length == 1) return input[0];
+
+        //suma elementów od lewej
+        int[] leftSum = new int[input.length];
+        leftSum[0] = input[0];
+        for (int i = 1; i < leftSum.length; i++) {
+            leftSum[i] = +leftSum[i - 1] + input[i];
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+        int actualSum = 0;
         for (int i = 0; i < input.length; i++) {
-            actualSum=0;
+            actualSum = leftSum[i];//suma elemntów do i-tego elementu
+            if (maxSum < actualSum) maxSum = actualSum;//czy to największa suma
+            for (int j = 0; j < i; j++) {
+                actualSum = leftSum[i]-leftSum[j];//suma elemntów od j do i-tego elementu // przedziały: (j-i)=(0-j)-(0-i)
+                if (maxSum < actualSum) maxSum = actualSum;//czy to największa suma
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static int solutionV1(int[] input) {
+        if (input.length == 1) return input[0];
+        int maxSum = Integer.MIN_VALUE;
+        int actualSum = 0;
+        for (int i = 0; i < input.length; i++) {
+            actualSum = 0;
             for (int j = i; j < input.length; j++) {
                 actualSum += input[j];
                 if (maxSum < actualSum) maxSum = actualSum;
@@ -53,7 +88,4 @@ public class MaxSliceSum {
         }
         return maxSum;
     }
-
-
-
 }
